ce0bb55a84aa907faddda16ab0e21a7ee7286bc8
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-07-20 17:46:48 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+// Enable CORS for all origins
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
